7b0f8dc399c09e6f3cd4c512f33b8a805b8b2fe2
Matias <matiasluduena23@gmail.com>
2024-02-19 17:12:35 -0300

    add responsiveness

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,15 +1,40 @@
 import { comments, currentUser } from "../data/data.json";
 import Comment from "./Comment";
 import commentReducer from "../reducer/commentReducer";
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import {
   CommetsDistpachContext,
   CommetsContext,
   CurrentUserContext,
 } from "../context/ComentContext";
+import Reply from "./Reply";
 
 export default function CommentsList() {
   const [tasks, dispatch] = useReducer(commentReducer, comments);
+  const [textarea, setTextarea] = useState();
+
+  const addComment = () => {
+    const newContent = formatContentString(textarea, username);
+
+    const newComment = {
+      content: newContent,
+      createdAt: "recently",
+      score: 0,
+      replies: [],
+      user: {
+        image: currentUser.image,
+        username: currentUser.username,
+      },
+    };
+
+    dispatch({
+      type: "addComment",
+      id,
+      newComment,
+    });
+    setActiveReply(false);
+    setTextarea("@" + username + ", ");
+  };
 
   return (
     <CommetsContext.Provider value={tasks}>
@@ -18,6 +43,11 @@ export default function CommentsList() {
           {tasks.map((comment) => (
             <Comment key={comment.id} comment={comment} />
           ))}
+          <Reply
+            textarea={textarea}
+            setTextarea={setTextarea}
+            handleReply={addComment}
+          />
         </CommetsDistpachContext.Provider>
       </CurrentUserContext.Provider>
     </CommetsContext.Provider>
--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -1,28 +1,26 @@
 /* eslint-disable react/prop-types */
-import { CurrentUserContext } from '../context/ComentContext';
-import { useContext } from 'react';
+import { CurrentUserContext } from "../context/ComentContext";
+import { useContext } from "react";
 
 export default function Reply({ handleReply, textarea, setTextarea }) {
-    const currentUser = useContext(CurrentUserContext);
+  const currentUser = useContext(CurrentUserContext);
+  const placeholder = textarea ? "" : "Add a comment...";
 
-    return (
-        <article className="reply">
-            <img
-                className="reply-img"
-                src={currentUser.image.png}
-                alt="image"
-            />
-            <textarea
-                className="reply-textarea"
-                name="postContent"
-                rows={3}
-                cols={30}
-                value={textarea}
-                onChange={(e) => setTextarea(e.target.value)}
-            />
-            <button className="reply-btn" onClick={handleReply}>
-                Reply
-            </button>
-        </article>
-    );
+  return (
+    <article className="reply">
+      <img className="reply-img" src={currentUser.image.png} alt="image" />
+      <textarea
+        className="reply-textarea"
+        name="postContent"
+        placeholder={placeholder}
+        rows={3}
+        cols={30}
+        value={textarea}
+        onChange={(e) => setTextarea(e.target.value)}
+      />
+      <button className="reply-btn" onClick={handleReply}>
+        Reply
+      </button>
+    </article>
+  );
 }
